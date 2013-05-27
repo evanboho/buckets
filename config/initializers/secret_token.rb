41620565b29dcb925f7987783c887ca6bb21b62d
Evan Kuchar <evan.kuchar@gmail.com>
2013-05-26 23:16:16 -0700

    ini commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buckets::Application.config.secret_key_base = 'c8807f8aef876b736d2786b4b9cd29e0d43a53713904b6492c9fc679cd64c6a1f243822b57e32764c6bb36bcc6aaa57a947ad62001f1e460e36578035290b217'
