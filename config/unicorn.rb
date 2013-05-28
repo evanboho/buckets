6283018cd8e09d5fdf9d445d7c6978071db8a2cb
Evan Kuchar <evan.kuchar@gmail.com>
2013-05-28 09:59:02 -0700

    change unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
+preload_app true
 listen "/tmp/unicorn.buckets.sock"
 worker_processes 2
 timeout 30
